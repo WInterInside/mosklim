eafc91d0c4c220475e6644ddf1a0e34f1996b701
Максим Циркулев <de_animator@mail.ru>
2024-11-07 12:58:46 +0300

    add popup and tabs scripts

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -22,8 +22,8 @@ document.addEventListener('click', (e) => {
 	}
 });
 
+// скролл шапки
 window.addEventListener("scroll", handleScroll);
-
 function handleScroll() {
 	if (window.scrollY > 0) {
 		body.classList.add('is-scrolled');
@@ -32,6 +32,7 @@ function handleScroll() {
 	}
 }
 
+// прокрутка статьи
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 	anchor.addEventListener('click', function(e) {
 		e.preventDefault();
@@ -44,3 +45,77 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 		}
 	});
 });
+
+// работа с попапом
+const openModalBtn = document.querySelector('[data-element="orderBtn"]');
+const modal = document.querySelector('[data-element="orderForm"]');
+const closeBtn = document.querySelector('[data-element="popupClose"]');
+const overlay = modal.querySelector('.popup__overlay'); // Добавляем ссылку на overlay
+
+openModalBtn.addEventListener('click', () => {
+	modal.classList.add('is-open');
+	body.classList.add('is-popup-open');
+});
+
+closeBtn.addEventListener('click', () => {
+	modal.classList.remove('is-open');
+	body.classList.remove('is-popup-open');
+});
+
+// Закрыть модальное окно при нажатии клавиши Escape
+document.addEventListener('keydown', (e) => {
+	if (e.key === 'Escape' && modal.classList.contains('is-open')) {
+		modal.classList.remove('is-open');
+		body.classList.remove('is-popup-open');
+	}
+});
+
+// Закрыть модальное окно при клике на overlay
+overlay.addEventListener('click', (e) => {
+	if (modal.classList.contains('is-open')) {
+		modal.classList.remove('is-open');
+	}
+});
+
+class Tabs {
+	setTabs = (tabs, id) => {
+		this.clearTabs(tabs);
+
+		tabs.querySelectorAll('[data-tab="' + id + '"]').forEach((el) => {
+			el.classList.add('is-active');
+		});
+	}
+
+	clearTabs = (tabs) =>{
+		let tabsBtn = tabs.querySelectorAll('[data-elements ~= "tabsBtn"]'),
+			tabsItems = tabs.querySelectorAll('[data-elements ~= "tabsItem"]');
+
+		tabsBtn.forEach((el) => {
+			el.classList.remove('is-active')
+		});
+
+		tabsItems.forEach((el) => {
+			el.classList.remove('is-active')
+		});
+	}
+}
+
+document.addEventListener('click', (e) => {
+	let el = e.target.closest('[data-elements~="tabsBtn"]');
+	if (el) {
+	  let id = el.dataset.tab;
+	  let tabs = el.closest('[data-elements~="tabs"]');
+	  let tabContents = tabs.querySelectorAll('[data-tab]');
+  
+	  tabContents.forEach((item) => {
+		item.classList.toggle('is-active', id === item.dataset.tab);
+	  });
+  
+	  // Example method to set the active tabs
+	  function setTabs(tabsContainer, activeTabId) {
+		console.log(`Setting tabs for ${activeTabId} in ${tabsContainer}`);
+	  }
+  
+	  setTabs(tabs, id);
+	}
+  });
