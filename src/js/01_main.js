a209c1c5e6c0d99982bece913ed850b676aae2eb
Максим Циркулев <de_animator@mail.ru>
2024-11-09 18:53:29 +0300

    upd order popup \ upd info tabs in item card page

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,8 +1,12 @@
 const body = document.body;
 const navToggle = document.querySelector('[data-element~="navToggle"]');
 const navMore = document.querySelector('.header__nav-more');
+const orderBtn = document.querySelector('[data-element="orderBtn"]');
+const modal = document.querySelector('[data-element="orderForm"]');
+const closeBtn = document.querySelector('[data-element="popupClose"]');
+const overlay = modal.querySelector('.popup__overlay');
+const swipeBtn = document.querySelector('[data-element="popupSwipe"]');
 
-// Function to toggle navigation
 const toggleNav = () => {
 	if (!body.classList.contains('is-nav-open')) {
 		body.classList.add('is-nav-open');
@@ -11,9 +15,23 @@ const toggleNav = () => {
 	}
 };
 
-// Add event listener to navToggle and navMore
+const handleScroll = () => {
+	(window.scrollY > 0) ?  body.classList.add('is-scrolled') : body.classList.remove('is-scrolled');
+}
+
+const closeModal = () => {
+	modal.classList.remove('is-open');
+	body.classList.remove('is-popup-open');
+}
+
+const openModal = () => {
+	modal.classList.add('is-open');
+	body.classList.add('is-popup-open');
+}
+
 navToggle.addEventListener('click', toggleNav);
 navMore.addEventListener('click', toggleNav);
+window.addEventListener("scroll", handleScroll);
 
 // Add event listener to document to close nav when clicking outside
 document.addEventListener('click', (e) => {
@@ -21,16 +39,6 @@ document.addEventListener('click', (e) => {
 		body.classList.remove('is-nav-open');
 	}
 });
-
-// скролл шапки
-window.addEventListener("scroll", handleScroll);
-function handleScroll() {
-	if (window.scrollY > 0) {
-		body.classList.add('is-scrolled');
-	} else {
-		body.classList.remove('is-scrolled');
-	}
-}
 
 // прокрутка статьи
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
@@ -46,36 +54,46 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 	});
 });
 
-// работа с попапом
-const openModalBtn = document.querySelector('[data-element="orderBtn"]');
-const modal = document.querySelector('[data-element="orderForm"]');
-const closeBtn = document.querySelector('[data-element="popupClose"]');
-const overlay = modal.querySelector('.popup__overlay'); // Добавляем ссылку на overlay
+if (orderBtn) {
+	orderBtn.addEventListener('click', () => {
+		openModal();
+	});
 
-openModalBtn.addEventListener('click', () => {
-	modal.classList.add('is-open');
-	body.classList.add('is-popup-open');
-});
+	closeBtn.addEventListener('click', () => {
+		closeModal();
+	});
 
-closeBtn.addEventListener('click', () => {
-	modal.classList.remove('is-open');
-	body.classList.remove('is-popup-open');
-});
+	// Закрыть модальное окно при нажатии клавиши Escape
+	document.addEventListener('keydown', (e) => {
+		if (e.key === 'Escape' && modal.classList.contains('is-open')) {
+			closeModal();
+		}
+	});
 
-// Закрыть модальное окно при нажатии клавиши Escape
-document.addEventListener('keydown', (e) => {
-	if (e.key === 'Escape' && modal.classList.contains('is-open')) {
-		modal.classList.remove('is-open');
-		body.classList.remove('is-popup-open');
+	// Закрыть модальное окно при клике на overlay
+	overlay.addEventListener('click', () => {
+		closeModal();
+	});
+	// Обработка свайпа по кнопке
+	let touchstartY = 0;
+	let touchendY = 0;
+
+	swipeBtn.addEventListener('touchstart', e => {
+		touchstartY = e.changedTouches[0].screenX;
+	});
+
+	swipeBtn.addEventListener('touchend', e => {
+		touchendY = e.changedTouches[0].screenX;
+		handleSwipe();
+	});
+
+	// Функция для обработки свайпа
+	function handleSwipe() {
+		if (touchendY < touchstartY) {
+			closeModal();
+		}
 	}
-});
-
-// Закрыть модальное окно при клике на overlay
-overlay.addEventListener('click', (e) => {
-	if (modal.classList.contains('is-open')) {
-		modal.classList.remove('is-open');
-	}
-});
+}
 
 class Tabs {
 	setTabs = (tabs, id) => {
@@ -103,19 +121,51 @@ class Tabs {
 document.addEventListener('click', (e) => {
 	let el = e.target.closest('[data-elements~="tabsBtn"]');
 	if (el) {
-	let id = el.dataset.tab;
-	let tabs = el.closest('[data-elements~="tabs"]');
-	let tabContents = tabs.querySelectorAll('[data-tab]');
+		let id = el.dataset.tab;
+		let tabs = el.closest('[data-elements~="tabs"]');
+		let tabContents = tabs.querySelectorAll('[data-tab]');
 
-	tabContents.forEach((item) => {
-		item.classList.toggle('is-active', id === item.dataset.tab);
-	});
+		tabContents.forEach((item) => {
+			item.classList.toggle('is-active', id === item.dataset.tab);
+		});
 
-	// Example method to set the active tabs
-	function setTabs(tabsContainer, activeTabId) {
-		console.log(`Setting tabs for ${activeTabId} in ${tabsContainer}`);
+		// Example method to set the active tabs
+		// function setTabs(tabsContainer, activeTabId) {
+		// 	console.log(`Setting tabs for ${activeTabId} in ${tabsContainer}`);
+		// }
+		// setTabs(tabs, id);
 	}
+});
 
-	setTabs(tabs, id);
+
+[].forEach.call( document.querySelectorAll('.form__input--tel'), function(input) {
+	var keyCode;
+	function mask(event) {
+		event.keyCode && (keyCode = event.keyCode);
+		var pos = this.selectionStart;
+		if (pos < 3) event.preventDefault();
+		var matrix = "+7 (___) ___ __ __",
+			i = 0,
+			def = matrix.replace(/\D/g, ""),
+			val = this.value.replace(/\D/g, ""),
+			new_value = matrix.replace(/[_\d]/g, function(a) {
+				return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+			});
+		i = new_value.indexOf("_");
+		if (i != -1) {
+			i < 5 && (i = 3);
+			new_value = new_value.slice(0, i)
+		}
+		var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+			function(a) {
+				return "\\d{1," + a.length + "}"
+			}).replace(/[+()]/g, "\\$&");
+		reg = new RegExp("^" + reg + "$");
+		if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+		if (event.type == "blur" && this.value.length < 5) this.value = ""
 	}
+	input.addEventListener("input", mask, false);
+	input.addEventListener("focus", mask, false);
+	input.addEventListener("blur", mask, false);
+	input.addEventListener("keydown", mask, false)
 });
